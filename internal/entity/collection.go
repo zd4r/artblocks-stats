8d9cb4cf69df47b3e33342d62e9305eb85db4f78
zd4rova <fakenotbad75@gmail.com>
2023-01-20 02:03:35 +0300

    add json tags

--- internal/entity/collection.go
+++ internal/entity/collection.go
@@ -1,22 +1,22 @@
 package entity
 
 type Collection struct {
-	ID                  int
-	HoldersCount        int
-	HoldersDistribution HoldersDistribution
-	Holders             []Holder
+	ID                  int                 `json:"id"`
+	HoldersCount        int                 `json:"holders_count"`
+	HoldersDistribution HoldersDistribution `json:"holders_distribution"`
+	Holders             []Holder            `json:"holders"`
 }
 
 type HoldersDistribution struct {
-	ByCommitmentScore map[string]int
-	ByPortfolioScore  map[string]int
-	ByTradingScore    map[string]int
+	ByCommitmentScore map[string]int `json:"by_commitment_score"`
+	ByPortfolioScore  map[string]int `json:"by_portfolio_score"`
+	ByTradingScore    map[string]int `json:"by_trading_score"`
 }
 
 type Holder struct {
-	Address         string
-	TokensAmount    int
-	CommitmentScore float64
-	PortfolioScore  float64
-	TradingScore    float64
+	Address         string  `json:"address"`
+	TokensAmount    int     `json:"tokens_amount"`
+	CommitmentScore float64 `json:"commitment_score"`
+	PortfolioScore  float64 `json:"portfolio_score"`
+	TradingScore    float64 `json:"trading_score"`
 }
