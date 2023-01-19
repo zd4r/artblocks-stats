60a73bf72129eba94860c5d14a70b65e5b6df7ab
zd4rova <fakenotbad75@gmail.com>
2023-01-20 02:04:11 +0300

    add http controller set up

---
+++ internal/controller/http/v1/collection.go
@@ -0,0 +1,52 @@
+package v1
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog"
+	"github.com/zd4rova/artblocks-holders/internal/entity"
+	"github.com/zd4rova/artblocks-holders/internal/usecase"
+)
+
+type collectionRoutes struct {
+	c usecase.Collection
+	l *zerolog.Logger
+}
+
+func newCollectionRoutes(handler *echo.Group, c usecase.Collection, l *zerolog.Logger) {
+	r := &collectionRoutes{c, l}
+
+	handler.GET("/collections/:id", r.collectionStats())
+}
+
+type envelope map[string]interface{}
+
+func (cr *collectionRoutes) collectionStats() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		id := c.Param("id")
+		collectionID, err := strconv.Atoi(id)
+		if err != nil {
+			cr.l.Err(err).Msg("http - v1 - collectionStats - strconv.Atoi")
+			return echo.NewHTTPError(http.StatusBadRequest, "invalid collection id")
+		}
+
+		collection := entity.Collection{
+			ID: collectionID,
+			HoldersDistribution: entity.HoldersDistribution{
+				ByCommitmentScore: make(map[string]int),
+				ByTradingScore:    make(map[string]int),
+				ByPortfolioScore:  make(map[string]int),
+			},
+		}
+
+		collectionStats, err := cr.c.Stats(c.Request().Context(), collection)
+		if err != nil {
+			cr.l.Err(err).Msg("http - v1 - collectionStats - cr.c.Stats")
+			return echo.NewHTTPError(http.StatusInternalServerError, "failed getting collection stats")
+		}
+
+		return c.JSON(http.StatusOK, envelope{"collection": collectionStats})
+	}
+}
---
+++ internal/controller/http/v1/router.go
@@ -0,0 +1,34 @@
+package v1
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog"
+	"github.com/zd4rova/artblocks-holders/internal/usecase"
+)
+
+func NewRouter(handler *echo.Echo, l *zerolog.Logger, c usecase.Collection) {
+
+	// Middleware
+	handler.Use(middleware.Logger())
+	handler.Use(middleware.Recover())
+	handler.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:    true,
+		LogStatus: true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			l.Info().
+				Str("URI", v.URI).
+				Int("status", v.Status).
+				Str("latency", v.Latency.String()).
+				Msg("request")
+
+			return nil
+		},
+	}))
+
+	// Routers
+	h := handler.Group("/v1")
+	{
+		newCollectionRoutes(h, c, l)
+	}
+}
