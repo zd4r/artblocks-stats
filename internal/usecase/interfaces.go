80b5bf0f3d26d3ade2675964d7b991cc49305a26
zd4rova <fakenotbad75@gmail.com>
2023-01-20 00:47:00 +0300

    add interfaces for dependency injection

---
+++ internal/usecase/interfaces.go
@@ -0,0 +1,19 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/zd4rova/artblocks-holders/internal/entity"
+)
+
+type (
+	CollectionWebAPI interface {
+		GetHoldersCount(entity.Collection) (entity.Collection, error)
+		GetHolders(entity.Collection) (entity.Collection, error)
+		GetHolderScores(entity.Holder) (entity.Holder, error)
+	}
+
+	Collection interface {
+		Stats(context.Context, entity.Collection) (entity.Collection, error)
+	}
+)
