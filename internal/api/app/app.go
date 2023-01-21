326ce5f9113ec1eb82ae70f9cdf3299133f41982
zd4rova <fakenotbad75@gmail.com>
2023-01-21 05:07:45 +0300

    update project structure

--- internal/api/app/app.go
+++ internal/api/app/app.go
@@ -1,28 +1,31 @@
 package app
 
 import (
+	"fmt"
+
 	"github.com/labstack/echo/v4"
 	"github.com/zd4rova/artblocks-holders/config"
 	v1 "github.com/zd4rova/artblocks-holders/internal/controller/http/v1"
 	"github.com/zd4rova/artblocks-holders/internal/usecase"
 	"github.com/zd4rova/artblocks-holders/internal/usecase/webapi"
 	"github.com/zd4rova/artblocks-holders/pkg/logger"
+	"github.com/zd4rova/artblocks-holders/pkg/postgres"
 )
 
 func Run(cfg *config.Config) {
 	// Logger
 	l := logger.New(cfg.Log.Level)
-	//l.Info().Msg("Hello world")
-	//
-	//e := echo.New()
-	//
-	//middleware.RequestID()
-	//
-	//e.GET("/", func(c echo.Context) error {
-	//	return c.String(http.StatusOK, "Hello, World!")
-	//})
-	//e.Use(middleware.Logger())
-	//e.Logger.Fatal(e.Start(":8080"))
+
+	// Repository
+	dbCfg := &postgres.Config{
+		MaxOpenConns: cfg.MaxOpenConns,
+		MaxIdleConns: cfg.MaxIdleConns,
+		MaxIdleTime:  cfg.MaxIdleTime,
+	}
+	_, err := postgres.New(dbCfg)
+	if err != nil {
+		l.Fatal().Err(fmt.Errorf("app - Run - postgres.New: %w", err))
+	}
 
 	// Use case
 	collectionUseCase := usecase.NewCollection(
@@ -30,6 +33,8 @@ func Run(cfg *config.Config) {
 	)
 
 	handler := echo.New()
+
 	v1.NewRouter(handler, l, collectionUseCase)
-	l.Fatal().Msg(handler.Start("8080").Error())
+
+	l.Fatal().Msg(handler.Start(":8080").Error())
 }
