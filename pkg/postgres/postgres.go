73eb716497d1520e4c104fd0244263e6ed1e109e
zd4rova <fakenotbad75@gmail.com>
2023-01-21 05:09:28 +0300

    add postgress pkg for future integration

---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,48 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+const (
+	pingTimeOut = 5
+)
+
+type Config struct {
+	URL string
+
+	MaxOpenConns int
+	MaxIdleConns int
+	MaxIdleTime  string
+}
+
+func New(cfg *Config) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres", cfg.URL)
+	if err != nil {
+		return nil, fmt.Errorf("postgres - New - sqlx.Open: %w", err)
+	}
+
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.MaxIdleTime)
+	if err != nil {
+		return nil, fmt.Errorf("postgres - New - time.ParseDuration: %w", err)
+	}
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), pingTimeOut*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("postgres - New - pg.Pool.PingContext: %w", err)
+	}
+
+	return db, nil
+}
