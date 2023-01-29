439b95b3367c28f062e7e662d13126370c743a2f
zd4rova <fakenotbad75@gmail.com>
2023-01-30 02:18:47 +0300

    update: user's scores are stored in db

--- internal/api/usecase/collection.go
+++ internal/api/usecase/collection.go
@@ -2,17 +2,21 @@ package usecase
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"time"
 
 	"github.com/zd4rova/artblocks-stats/internal/api/entity"
 )
 
 type CollectionUseCase struct {
+	repo   HoldersRepo
 	webAPI CollectionWebAPI
 }
 
-func NewCollection(w CollectionWebAPI) *CollectionUseCase {
+func NewCollection(r HoldersRepo, w CollectionWebAPI) *CollectionUseCase {
 	return &CollectionUseCase{
+		repo:   r,
 		webAPI: w,
 	}
 }
@@ -21,22 +25,47 @@ func (uc *CollectionUseCase) СalculateStats(ctx context.Context, c entity.Colle
 
 	collection, err := uc.webAPI.GetHoldersCount(c)
 	if err != nil {
-		return entity.Collection{}, fmt.Errorf("CollectionUseCase - GetHoldersCount - uc.webAPI.GetHolders: %w", err)
+		return entity.Collection{}, fmt.Errorf("CollectionUseCase - СalculateStats - uc.webAPI.GetHoldersCount: %w", err)
 	}
 
 	collection.Holders = make([]entity.Holder, collection.HoldersCount)
 
 	collection, err = uc.webAPI.GetHolders(collection)
 	if err != nil {
-		return entity.Collection{}, fmt.Errorf("CollectionUseCase - Stats - uc.webAPI.GetHolders: %w", err)
+		return entity.Collection{}, fmt.Errorf("CollectionUseCase - СalculateStats - uc.webAPI.GetHolders: %w", err)
 	}
 
 	for i, h := range collection.Holders {
-		holder, err := uc.webAPI.GetHolderScores(h)
+		holder, err := uc.repo.Get(h)
 		if err != nil {
-			return entity.Collection{}, fmt.Errorf("CollectionUseCase - Stats - uc.webAPI.GetHolderScores: %w", err)
+			switch {
+			case errors.Is(err, entity.ErrHolderNotFound):
+				holder, err = uc.webAPI.GetHolderScores(h)
+				if err != nil {
+					return entity.Collection{}, fmt.Errorf("CollectionUseCase - СalculateStats - uc.webAPI.GetHolderScores: %w", err)
+				}
+
+				holder, err = uc.repo.Insert(holder)
+				if err != nil {
+					return entity.Collection{}, fmt.Errorf("CollectionUseCase - СalculateStats - uc.repo.Insert: %w", err)
+				}
+			default:
+				return entity.Collection{}, fmt.Errorf("CollectionUseCase - СalculateStats - us.repo.Get: %w", err)
+			}
 		}
 
+		// TODO: Fix: expiration date hardcode
+		if time.Now().Sub(holder.UpdatedAt) >= 72*time.Hour {
+			holder, err = uc.webAPI.GetHolderScores(h)
+			if err != nil {
+				return entity.Collection{}, fmt.Errorf("CollectionUseCase - СalculateStats - uc.webAPI.GetHolderScores: %w", err)
+			}
+
+			holder, err = uc.repo.UpdateScores(holder)
+			if err != nil {
+				return entity.Collection{}, fmt.Errorf("CollectionUseCase - СalculateStats - uc.repo.UpdateScores: %w", err)
+			}
+		}
 		collection.Holders[i] = holder
 	}
 
