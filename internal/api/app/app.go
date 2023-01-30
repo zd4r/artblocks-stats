5f5338c5d454c148953fd492665cdd48cc93c5c5
zd4rova <fakenotbad75@gmail.com>
2023-01-30 21:53:55 +0300

    add: graceful shutdown implementation

--- internal/api/app/app.go
+++ internal/api/app/app.go
@@ -2,39 +2,65 @@ package app
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/labstack/echo/v4"
-	"github.com/zd4rova/artblocks-holders/config"
-	v1 "github.com/zd4rova/artblocks-holders/internal/controller/http/v1"
-	"github.com/zd4rova/artblocks-holders/internal/usecase"
-	"github.com/zd4rova/artblocks-holders/internal/usecase/webapi"
-	"github.com/zd4rova/artblocks-holders/pkg/logger"
-	"github.com/zd4rova/artblocks-holders/pkg/postgres"
+	_ "github.com/lib/pq"
+	"github.com/zd4rova/artblocks-stats/cmd/api/config"
+	"github.com/zd4rova/artblocks-stats/internal/api/controller/http/v1"
+	"github.com/zd4rova/artblocks-stats/internal/api/usecase"
+	"github.com/zd4rova/artblocks-stats/internal/api/usecase/repo"
+	"github.com/zd4rova/artblocks-stats/internal/api/usecase/webapi"
+	"github.com/zd4rova/artblocks-stats/pkg/httpserver"
+	"github.com/zd4rova/artblocks-stats/pkg/logger"
+	"github.com/zd4rova/artblocks-stats/pkg/postgres"
 )
 
 func Run(cfg *config.Config) {
 	// Logger
-	l := logger.New(cfg.Log.Level)
+	l := logger.New(cfg.Log.Level, cfg.Log.Structured)
+
+	l.Info().Msg(fmt.Sprintf("⚡ init app [ name: %v, version: %v ]", cfg.Name, cfg.Version))
 
 	// Repository
 	dbCfg := &postgres.Config{
-		MaxOpenConns: cfg.MaxOpenConns,
-		MaxIdleConns: cfg.MaxIdleConns,
-		MaxIdleTime:  cfg.MaxIdleTime,
+		URL:          cfg.PG.URL,
+		MaxOpenConns: cfg.PG.MaxOpenConns,
+		MaxIdleConns: cfg.PG.MaxIdleConns,
+		MaxIdleTime:  cfg.PG.MaxIdleTime,
 	}
-	_, err := postgres.New(dbCfg)
+	pg, err := postgres.New(dbCfg)
 	if err != nil {
-		l.Fatal().Err(fmt.Errorf("app - Run - postgres.New: %w", err))
+		l.Fatal().Msg(fmt.Errorf("app - Run - postgres.New: %w", err).Error())
 	}
 
 	// Use case
 	collectionUseCase := usecase.NewCollection(
+		repo.New(pg.DB),
 		webapi.New(),
 	)
 
+	// HTTP server
 	handler := echo.New()
-
 	v1.NewRouter(handler, l, collectionUseCase)
+	httpServer := httpserver.New(handler, httpserver.Port(cfg.HTTP.Port))
+	l.Info().Msg(fmt.Sprintf("🌏 http server started on :%v", cfg.HTTP.Port))
 
-	l.Fatal().Msg(handler.Start(":8080").Error())
+	// Graceful shutdown
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case s := <-interrupt:
+		l.Info().Msg(fmt.Sprintf("app - Run - signal: %v", s.String()))
+	case err = <-httpServer.Notify():
+		l.Err(err).Msg("app - Run - httpServer.Notify")
+	}
+
+	err = httpServer.Shutdown()
+	if err != nil {
+		l.Err(err).Msg("app - Run - httpServer.Shutdown")
+	}
 }
